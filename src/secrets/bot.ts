aa27ae7cc4ebad4b9f5b4adfb872060ddd5b4aff
Aecxlol <aubry_damien@orange.fr>
2021-02-21 14:21:58 +0100

    api start

---
+++ src/secrets/bot.ts
@@ -0,0 +1,14 @@
+import {options} from '../Interface/BotOptionsInterface';
+
+/**
+ * Define configuration options
+ */
+export const ENV_VAR: options = {
+    identity: {
+        username: 'yourBotName',
+        password: 'yourToken',
+    },
+    channels: [
+        'yourChannel'
+    ]
+}
---
+++ src/secrets/database.ts
@@ -0,0 +1,12 @@
+import {dbConf} from '../Interface/DbConfInterface';
+
+/**
+ * Define the database's configuration
+ */
+export const DB_CONF: dbConf = {
+    connectionLimit: 10,
+    host: '',
+    user: '',
+    password: '',
+    database:''
+}
